61ab5c42f206b0077fbed3ffd69b3595bbacbaa7
boudrouzaissam <boudrouzaissam@gmail.com>
2026-06-03 01:04:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
 
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
